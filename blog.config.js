f73bb48b6ee6a37b38c4f607853570b03d1d0538
Fanny Dwi Cahyanto <107551048+bismillahwin@users.noreply.github.com>
2025-05-06 19:51:55 +0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Fanny Dwi Cahyanto',
   author: 'Funny DC',
   email: 'fannydwi567@gmail.com',
-  link: 'https://funnydc.vercel.app',
+  link: 'https://funnydc.vercel.app/',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Jakarta', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
